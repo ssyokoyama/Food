f830e2e9d00e416b283a4a024ae22a801012c786
yokoyama <startnow7240@gmail.com>
2019-05-06 02:08:18 +0900

    課題アプリ制作190506

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_150200) do
+ActiveRecord::Schema.define(version: 2019_05_05_135137) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
